fb40ab7c041cda76865bfc6bd2ad3eb7d7ee08e5
natmsaenz <natalia.madrigal2941@alumnos.udg.mx>
2024-04-03 23:17:17 -0600

    Started creating the download button component

---
+++ personal-blog/frontend/src/components/download-resume/download-resume.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import './download-resume.css'
+
+
+const DownloadResumeButton = () =>{
+return(
+    <>
+    <button className="DownloadResume">Download my resume</button>
+    </>
+)
+}
+
+export default DownloadResumeButton
