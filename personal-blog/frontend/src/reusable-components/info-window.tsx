266b4fd63c3b8a055b3f8b6fe234a7a01ad8033a
natmsaenz <natalia.madrigal2941@alumnos.udg.mx>
2024-04-12 22:11:54 -0600

    Almost finished the InfoWindow component, few fixes left

--- personal-blog/frontend/src/reusable-components/info-window.tsx
+++ personal-blog/frontend/src/reusable-components/info-window.tsx
@@ -14,5 +14,34 @@ const InformationWindow: React.FC<InformationWindowProps> = ({
   title,
   description,
 }) => {
-    const [isMinimized, setIsMinimized] = useState(false)
+  const [minimized, setIsMinimized] = useState(false);
+
+  const handleClose = () => {
+    setIsMinimized(false);
+    isClosed();
+  };
+
+  const handleMinimize = () => {
+    setIsMinimized(true);
+    isMinimized();
+  };
+
+  if (!isOpen && !isMinimized) return null;
+
+  return (
+    <div className="InformationWindow">
+      <div className="InformationWindowContent">
+        <div className="InformationWindowHeader">
+          <div className="InformationWindowButtons">
+            <button onClick={handleMinimize}></button>
+            <button onClick={handleClose}></button>
+          </div>
+          <h2 className="InformationWindowTitle">{title}</h2>
+        </div>
+        <p className="InformationWindowDescription">{description}</p>
+      </div>
+    </div>
+  );
 };
+
+export default InformationWindow;
